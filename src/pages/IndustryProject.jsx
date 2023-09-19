9c702a60330d25e64010c67068f4061fc8d19f99
Ashish Sharma <sharmaashish049@gmail.com>
2023-09-19 17:03:49 +0530

    Final Changes

--- src/pages/IndustryProject.jsx
+++ src/pages/IndustryProject.jsx
@@ -13,7 +13,7 @@ const IndustryProject = () => {
         </header>
       </Fade>
       <main>
-        <div className="flex justify-center gap-5">
+        <div className="flex justify-center gap-5 flex-col">
           <Fade top cascade>
             <div className="w-100 card flex flex-col gap-5">
               <div className="inner-card shadow-sm hover:shadow-md cursor-pointer transition-all bg-slate-100 rounded-sm p-5">
@@ -45,6 +45,92 @@ const IndustryProject = () => {
               </div>
             </div>
           </Fade>
+          <header>
+            <h2 className="text-center lg:text-7xl text-4xl font-bold my-4">
+              Certificates
+            </h2>
+          </header>
+          <div className="w-100 card flex gap-5">
+            <div className="inner-card shadow-sm hover:shadow-md cursor-pointer transition-all bg-slate-100 rounded-sm p-5">
+              <h3 className="text-2xl font-bold">
+                Business Plan Challenge with E-Cell IIT Roorkee
+              </h3>
+              <p className="text-base mt-3">
+                Certificate of participation in the Business Plan Challenge with
+                E-Cell IIT Roorkee
+              </p>
+              <button>
+                <a
+                  className="px-4 py-2 mt-4 inline-block bg-black text-white"
+                  href="#"
+                >
+                  Know More
+                </a>
+              </button>
+            </div>
+            <div className="inner-card shadow-sm hover:shadow-md cursor-pointer transition-all bg-slate-100 rounded-sm p-5">
+              <h3 className="text-2xl font-bold">
+                JuniorMBA in Digital Marketing with a company project by Puma
+              </h3>
+              <p className="text-base mt-3">
+                Certificate of achievement in Digital Marketing with a company
+                project by PUMA
+              </p>
+              <button>
+                <a
+                  className="px-4 py-2 mt-4 inline-block bg-black text-white"
+                  href="#"
+                >
+                  Know More
+                </a>
+              </button>
+            </div>
+            <div className="inner-card shadow-sm hover:shadow-md cursor-pointer transition-all bg-slate-100 rounded-sm p-5">
+              <h3 className="text-2xl font-bold">
+                JuniorMBA in Entrepreneurship company project by Multiply
+                Ventures
+              </h3>
+              <p className="text-base mt-3">
+                Certificate of achievement in Entrepreneurship with a company
+                project by Multiply Ventures
+              </p>
+              <button>
+                <a
+                  className="px-4 py-2 mt-4 inline-block bg-black text-white"
+                  href="#"
+                >
+                  Know More
+                </a>
+              </button>
+            </div>
+            <div className="inner-card shadow-sm hover:shadow-md cursor-pointer transition-all bg-slate-100 rounded-sm p-5">
+              <h3 className="text-2xl font-bold">
+                Case Study: Entrepreneurship with a company project by Multiply
+                Ventures
+              </h3>
+              <button>
+                <a
+                  className="px-4 py-2 mt-4 inline-block bg-black text-white"
+                  href="#"
+                >
+                  Know More
+                </a>
+              </button>
+            </div>
+            <div className="inner-card shadow-sm hover:shadow-md cursor-pointer transition-all bg-slate-100 rounded-sm p-5">
+              <h3 className="text-2xl font-bold">
+                Digital Marketing with a company project by PUMA
+              </h3>
+              <button>
+                <a
+                  className="px-4 py-2 mt-4 inline-block bg-black text-white"
+                  href="#"
+                >
+                  Know More
+                </a>
+              </button>
+            </div>
+          </div>
         </div>
       </main>
     </div>
